3c4470a072392ac33e7cab0304ddd975db5dbf08
Kanagaraj-123 <rkraji105@gmail.com>
2026-03-21 11:33:35 +0000

    frontend_app_first

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>My First React App 🚀</h1>
+      <p>Frontend is working!</p>
+    </div>
+  );
+}
+
+export default App;
